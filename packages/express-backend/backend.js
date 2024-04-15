6c9fb782deaa5ee850bdd81686a31807effe96ee
Cole Costa <colecosta7@gmail.com>
2024-04-15 13:55:56 -0700

    added find user id logic

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -36,12 +36,27 @@ const port = 8000;
 
 app.use(express.json());
 
-app.get("/users", (req, res) => {
-  res.send(users);
+const findUserByName = (name) => {
+    return users["users_list"].filter(
+      (user) => user["name"] === name
+    );
+  };
+  
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
 });
 
 app.listen(port, () => {
   console.log(
-    `Example app listening at http://localhost:${port}/users`
+    `Example app listening at http://localhost:${port}/users/zap555`
   );
 });
