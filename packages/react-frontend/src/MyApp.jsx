3866ff8731c75d49e8144783c84fa8a2f8de1cb7
Cole Costa <colecosta7@gmail.com>
2024-04-19 23:57:57 -0700

    added IE3 changes

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,7 +1,8 @@
 // src/MyApp.jsx
-import React, {useState} from "react";
 import Table from "./Table";
 import Form from "./Form";
+import React, {useState, useEffect} from 'react';
+
 
 // const characters = [
 //     {
@@ -32,15 +33,61 @@ function MyApp() {
         setCharacters(updated);
     }
 
-    function updateList(person){
-        setCharacters([...characters, person]);
+    function updateList(person) {
+        postUser(person)
+          .then(() => setCharacters([...characters, person]))
+          .catch((error) => {
+            console.log(error);
+          })
     }
+
+    function fetchUsers() {
+        const promise = fetch("http://localhost:8000/users");
+        return promise;
+    }
+
+    function postUser(person) {
+        const promise = fetch("Http://localhost:8000/users", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(person),
+        });
+    
+        return promise;
+    }
+
+    function deleteUser(id){
+        const promise = fetch(`http://localhost:8000/users/${id}`, {
+            method: 'DELETE',
+        })
+            .then(() => {
+                return fetchUsers();
+            })
+            .then((res) => res.json())
+            .then((json) => {
+                setCharacters(json["users_list"]);
+            })
+            .catch((error) => {
+                console.log(error);
+            });  
+            
+        return promise;
+    }
+
+    useEffect(() => {
+        fetchUsers()
+            .then((res) => res.json())
+            .then((json) => setCharacters(json["users_list"]))
+            .catch((error) => { console.log(error); });
+      }, [] );
 
     return (
         <div className="container">
             <Table 
                 characterData = {characters}
-                removeCharacter = {removeOneCharacter}
+                removeCharacter = {deleteUser}
             />
             <Form handleSubmit = {updateList}/>
         </div>
